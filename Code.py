732f4ec506cfa772474d86e81e325fd6ce25c4d4
Rahul Oberoi <118175583+oberoir080@users.noreply.github.com>
2023-02-02 19:00:04 +0530

    Pipes added and bird moving forward, Hitboxes added but collision not added

--- Code.py
+++ Code.py
@@ -18,6 +18,74 @@ BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join("Assets","b
 PIPE = pygame.transform.scale2x(pygame.image.load(os.path.join("Assets","pipe.png")))
 GROUND = pygame.transform.scale2x(pygame.image.load(os.path.join("Assets","base.png")))
 
+class Pipe:
+    PIPE_GAP = 200
+    TERRAIN_VELOCITY = 5
+
+    def __init__(self,x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.UPPER_PIPE = pygame.transform.flip(PIPE, False,True)
+        self.BELOW_PIPE = PIPE
+
+        self.passed = False 
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50,450)
+        self.top =  self.height - self.UPPER_PIPE.get_height()
+        self.bottom = self.height + self.PIPE_GAP
+
+    def move(self):
+        self.x -= self.TERRAIN_VELOCITY
+
+    def draw(self,window):
+        window.blit(self.UPPER_PIPE, (self.x ,self.top))
+        window.blit(self.BELOW_PIPE, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        upper_mask = pygame.mask.from_surface(self.UPPER_PIPE)
+        below_mask = pygame.mask.from_surface(self.BELOW_PIPE)
+
+        upper_offset = (self.x - bird.x, self.top - round(bird.y))
+        below_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        point_of_collision_upper = bird_mask.overlap(upper_mask, upper_offset)
+        point_of_collision_below = bird_mask.overlap(below_mask, below_offset)
+
+        if point_of_collision_upper or point_of_collision_below:
+            return True
+
+        return False
+
+
+class Ground:
+    TERRAIN_VELOCITY = 5
+    WIDTH = GROUND.get_width()
+    
+    def __init__(self,y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.TERRAIN_VELOCITY
+        self.x2 -= self.TERRAIN_VELOCITY
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+        
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, window):
+        window.blit(GROUND, (self.x1, self.y))
+        window.blit(GROUND, (self.x2, self.y))
+        
 class Bird:
     MAX_ROTATION = 25
     ROTATION_VELOCITY = 20
@@ -90,16 +158,26 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
-def draw_window(window,bird):
+def draw_window(window, bird, pipes, ground):
     window.blit(BACKGROUND,(0,0)) #Drawing the background
+
+    for pipe in pipes:
+        pipe.draw(window)
+
+    ground.draw(window)
     bird.draw(window)
     pygame.display.update() #Updating for each frame
 
 def main():
     bird = Bird(210,320)
+    ground = Ground(730)
+    pipes = [Pipe(700)]
+
     window = pygame.display.set_mode((WIDTH,HEIGHT))
     clock = pygame.time.Clock()
     
+    score = 0
+
     run = True
     
     while run:
@@ -108,8 +186,32 @@ def main():
             if event.type == pygame.QUIT:
                 run=False
 
-        bird.move()
-        draw_window(window,bird)
+        # bird.move()
+        add_pipe = False
+        removed = []
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+
+            if pipe.x + pipe.UPPER_PIPE.get_width() < 0:
+                removed.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score+=1
+            pipes.append(Pipe(700))
+
+        for i in removed:
+            pipes.remove(i)
+
+        ground.move()
+
+        draw_window(window, bird, pipes, ground)
 
     pygame.quit()
     quit()
